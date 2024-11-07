a7feece8904190b0b15ffc5210fce9f1685307c2
Jan Christopher Francisco <152581275+jcfrancisco0103@users.noreply.github.com>
2024-11-07 18:00:20 +0000

    added

--- prototype/prototype/settings.py
+++ prototype/prototype/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'members',
+    'django.contrib.sites',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'merss0102@gmail.com'  # Replace with your Gmail address
+EMAIL_HOST_PASSWORD = 'bodw jnco pwlg fpyd'  # Replace with your Gmail password
